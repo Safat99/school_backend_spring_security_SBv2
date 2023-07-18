5dd342812908e893f65ba2ef2ffd249a0b323834
Safat99 <sarkersafat99@gmail.com>
2023-07-18 17:34:47 +0600

    entity modified

--- src/main/java/com/example/spring_security_5/entity/User.java
+++ src/main/java/com/example/spring_security_5/entity/User.java
@@ -17,10 +17,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
     private String email;
     private String password;
+
     @Enumerated(EnumType.STRING)
-    private Role role;
+    private Roles role;
+
     private String extra_info;
 }
--- src/main/java/com/example/spring_security_5/entity/UserRoles.java
+++ src/main/java/com/example/spring_security_5/entity/UserRoles.java
@@ -1,4 +1,22 @@
 package com.example.spring_security_5.entity;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@RequiredArgsConstructor
+@Data
+@Builder
 public class UserRoles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Roles role;
+
 }
